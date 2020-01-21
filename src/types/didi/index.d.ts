1ce3b767959b4ba0a0b41ad39c70a0ca60af17f3
Andrew Blomquist <andrew.blomquist@fmr.com>
2020-01-21 15:51:17 -0700

    more changes to didi, should try forking the repo and testing these types out on the unit tests

--- src/types/didi/index.d.ts
+++ src/types/didi/index.d.ts
@@ -2,6 +2,10 @@
 
 declare module 'didi' {
   type ProvidersArray = [string, 'factory' | 'value' | 'type', any][];
+  type unknownFunction = (...args: any[]) => any;
+  type unknownClass = new (...args: any[]) => any;
+  type unknownFunctionOrClass = unknownFunction | unknownClass;
+
   export class Module {
     factory(name: string, factory: any): Module;
     value(name: string, value: any): Module;
@@ -10,7 +14,7 @@ declare module 'didi' {
   }
 
   export class Injector {
-    constructor(modules: Module[], parent: Injector);
+    constructor(modules: (Module | object)[], parent?: Injector);
     /**
      * @param name    the name of the service
      * @param strict  defaults to true. If false, resolve missing services to null
@@ -20,8 +24,20 @@ declare module 'didi' {
      * @param context defaults to the global object in non-strict mode when null or undefined
      * @param locals  defaults to an empty object
      */
-    invoke(func: (...args) => any, context?: object, locals?: object): any;
-    instantiate(Type: (...args) => any): any;
+    invoke(func: unknownFunction, context?: object, locals?: object): any;
+    instantiate(Type: unknownFunction): any;
     createChild(modules: Module[], forceNewInstances: string[]): Injector;
   }
+
+  // annotation.js
+  /**
+   * Accepts a variable amount of arguments. The last argument should be the function to annotate.
+   * The first argument can be an array of arguments.
+   * Otherwise, each argument except the last is treated as arguments for the function to annotate.
+   *
+   * @returns the passed function
+   */
+  export function annotate(args: any[], fn: unknownFunctionOrClass): unknownFunctionOrClass;
+  export function annotate(...args: any[]): unknownFunctionOrClass;
+  export function parseAnnotations(fn: unknownFunctionOrClass): any[];
 }
